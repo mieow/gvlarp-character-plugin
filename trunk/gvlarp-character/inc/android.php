29e826c482fcb8cb7dffb593d2b3399fe558ef2b
mieowcat@gmail.com <mieowcat@gmail.com@5e93ad26-8c2b-7d49-301a-79fe4ed686b2>
2013-08-28 15:10:20 +0000

    plugin: some XML generation added for android app

--- trunk/gvlarp-character/inc/android.php
+++ trunk/gvlarp-character/inc/android.php
@@ -27,11 +27,21 @@ function gv_android_redirect () {
 		echo "<CHARACTER>\n";
 
 		/* Character Info */
-		echo output_xlmtag("NAME",       $mycharacter->name);
-		echo output_xlmtag("PLAYER",     $mycharacter->player);
-		echo output_xlmtag("GENERATION", $mycharacter->generation);
+		echo output_xlmtag("NAME",         $mycharacter->name);
+		echo output_xlmtag("PLAYER",       $mycharacter->player);
+		echo output_xlmtag("GENERATION",   $mycharacter->generation);
 		echo output_xlmtag("PUBLIC_CLAN",  $mycharacter->clan);
 		echo output_xlmtag("PRIVATE_CLAN", $mycharacter->private_clan);
+		echo output_xlmtag("CURRENT_XP",   $mycharacter->current_experience);
+		echo output_xlmtag("CURRENT_WP",   $mycharacter->current_willpower);
+		echo output_xlmtag("WILLPOWER",    $mycharacter->willpower);
+		echo output_xlmtag("BLOODPOOL",    $mycharacter->bloodpool);
+		echo output_xlmtag("PATH_OF_ENLIGHTENMENT", $mycharacter->path_of_enlightenment);
+		echo output_xlmtag("PATH_LEVEL",   $mycharacter->path_rating);
+		echo output_xlmtag("DOB",          $mycharacter->date_of_birth);
+		echo output_xlmtag("DOE",          $mycharacter->date_of_embrace);
+		echo output_xlmtag("SIRE",         $mycharacter->sire);
+		echo output_xlmtag("CLAN_FLAW",    $mycharacter->clan_flaw);
 		
 		/* Attributes */
 		echo "\t<ATTRIBUTES>\n";
@@ -80,6 +90,56 @@ function gv_android_redirect () {
 		}
 		echo "\t</ABILITIES>\n";
 		
+		/* Backgrounds */
+		$backgrounds =  $mycharacter->getBackgrounds();
+		echo "\t<BACKGROUNDS>\n";
+		foreach ($backgrounds as $background) {
+			echo "\t\t<BACKGROUND>\n";
+			echo output_xlmtag("NAME",       $background->background);
+			echo output_xlmtag("LEVEL",      $background->level);
+			echo output_xlmtag("SECTOR",     $background->sector);
+			echo output_xlmtag("COMMENT",    $background->comment);
+			echo "\t\t</BACKGROUND>\n";
+		}
+		echo "\t</BACKGROUNDS>\n";
+		
+		/* Disciplines */
+		$disciplines =  $mycharacter->getDisciplines();
+		echo "\t<DISCIPLINES>\n";
+		foreach ($disciplines as $discipline) {
+			echo "\t\t<DISCIPLINE>\n";
+			echo output_xlmtag("NAME",       $discipline->name);
+			echo output_xlmtag("LEVEL",      $discipline->level);
+			echo "\t\t</DISCIPLINE>\n";
+		}
+		echo "\t</DISCIPLINES>\n";
+
+		/* Merits and Flaws */
+		$merits =  $mycharacter->meritsandflaws;
+		echo "\t<MERITSANDFLAWS>\n";
+		foreach ($merits as $merit) {
+			echo "\t\t<MERITFLAW>\n";
+			echo output_xlmtag("NAME",       $merit->name);
+			echo output_xlmtag("LEVEL",      $merit->level);
+			echo output_xlmtag("COMMENT",    $merit->comment);
+			echo "\t\t</MERITFLAW>\n";
+		}
+		echo "\t</MERITSANDFLAWS>\n";
+
+		/* Virtues */
+		$virtues =  $mycharacter->getAttributes("Virtue");
+		echo "\t<VIRTUES>\n";
+		foreach ($virtues as $virtue) {
+			echo "\t\t<VIRTUE>\n";
+			echo output_xlmtag("NAME",       $virtue->name);
+			echo output_xlmtag("LEVEL",      $virtue->level);
+			echo output_xlmtag("ORDER",      $virtue->ordering);
+			echo "\t\t</VIRTUE>\n";
+		}
+		echo "\t</VIRTUES>\n";
+		
+		
+		
 		echo "</CHARACTER>\n";
 		
 		exit;
@@ -87,7 +147,7 @@ function gv_android_redirect () {
 }
 
 function output_xlmtag ($tagname, $value) {
-	return "\t\t\t\t<$tagname>$value</$tagname>\n";
+	return (empty($value) ? "" : "\t\t\t\t<$tagname>$value</$tagname>\n");
 }
 
 function get_gvconfig ($field) {
