50b3c663a6438e18041d01aeacb73690177029d9
mieowcat@gmail.com <mieowcat@gmail.com@5e93ad26-8c2b-7d49-301a-79fe4ed686b2>
2013-09-10 14:58:11 +0000

    fixed forms - doesn't lose stuff in text boxes when 1 section is submitted

--- gvlarp-character/inc/extendedbackground.php
+++ gvlarp-character/inc/extendedbackground.php
@@ -2,9 +2,12 @@
 
 function gv_extended_background_content_filter($content) {
 
-  if (is_page(get_stlink_page('viewExtBackgrnd')) && is_user_logged_in()) {
-    $content .= get_extbackgrounds_content();
-  }
+  if (is_page(get_stlink_page('viewExtBackgrnd')))
+		if (is_user_logged_in()) {
+			$content .= get_extbackgrounds_content();
+		} else {
+			$content .= "<p>You must be logged in to view this content.</p>";
+		}
   // otherwise returns the database content
   return $content;
 }
@@ -71,6 +74,8 @@ function get_highlight($tab, $default="backgrounds"){
 function get_tabdisplay($tab, $default="backgrounds") {
 
 	$display = "style='display:none'";
+	
+	/* echo "<p>tab: $tab, request tab: {$_REQUEST['tab']}.</p>"; */
 
 	if (isset($_REQUEST['tab'])) {
 		if ($_REQUEST['tab'] == $tab)
@@ -89,27 +94,37 @@ function get_editbackgrounds_tab($characterID) {
 	$characterID = establishCharacterID($character);
 	
 	/* Save backgrounds */
-	if (isset($_REQUEST['charbgID'])) {
-		$data = array (
-			'SECTOR_ID' => $_REQUEST['sectorid'],
-			'PENDING_DETAIL' => $_REQUEST['pending'],
-			'DENIED_DETAIL'  => ''
-		);
-		$wpdb->show_errors();
-		$result = $wpdb->update(GVLARP_TABLE_PREFIX . "CHARACTER_BACKGROUND",
-			$data,
-			array (
-				'ID' => $_REQUEST['charbgID']
-			)
-		);
+	if (isset($_REQUEST['save_bgform'])) {
+	
+		$bgids     = $_REQUEST['charbgID'];
+		$sectors   = $_REQUEST['sectorid'];
+		$pendingbg = $_REQUEST['pendingbg'];
+		$namesbg     = $_REQUEST['charbgName'];
 		
-		if ($result) 			echo "<p style='color:green'>Updated {$_REQUEST['charbgName']} background</p>";
-		else if ($result === 0) echo "<p style='color:orange'>No updates made to {$_REQUEST['charbgName']} background</p>";
-		else {
-			$wpdb->print_error();
-			echo "<p style='color:red'>Could not update {$_REQUEST['charbgName']} background</p>";
-		}
-		
+		foreach ($_REQUEST['save_bgform'] as $id => $buttontitle) {
+
+			$data = array (
+				'SECTOR_ID' => $sectors[$id],
+				'PENDING_DETAIL' => $pendingbg[$id],
+				'DENIED_DETAIL'  => ''
+			);
+			$wpdb->show_errors();
+			$result = $wpdb->update(GVLARP_TABLE_PREFIX . "CHARACTER_BACKGROUND",
+				$data,
+				array (
+					'ID' => $bgids[$id]
+				)
+			);
+			
+			if ($result) 			echo "<p style='color:green'>Updated {$namesbg[$id]} background</p>";
+			else if ($result === 0) echo "<p style='color:orange'>No updates made to {$namesbg[$id]} background</p>";
+			else {
+				$wpdb->print_error();
+				echo "<p style='color:red'>Could not update {$namesbg[$id]} background</p>";
+			}
+			
+			}
+			
 	}
 
 	/* get all the backgrounds for this character that need extra detail */
@@ -128,6 +143,10 @@ function get_editbackgrounds_tab($characterID) {
 	
 	$backgrounds = $wpdb->get_results($wpdb->prepare($sql, $characterID));
 	$i = 0;
+	
+	$content .= "<form name='extbgform' method='post'>\n";
+	$content .= "<input type='hidden' name='charID' value='$characterID' />";
+	
 	foreach ($backgrounds as $background) {
 		$content .= "<p class='gvext_name'>" . $background->NAME . ": " . $background->LEVEL;
 		$content .= ($background->COMMENT) ? " ({$background->COMMENT})" : "";
@@ -135,14 +154,13 @@ function get_editbackgrounds_tab($characterID) {
 		if (!empty($background->BACKGROUND_QUESTION))
 			$content .= "<p class='gvext_ques'>" . stripslashes($background->BACKGROUND_QUESTION) . "</p>\n";
 		$content .= "<div class='gvext_section'>";
-		$content .= "<form name='extbgform{$i}' method='post'>\n";
-		$content .= "<input type='hidden' name='charID' value='$characterID' />";
-		$content .= "<input type='hidden' name='charbgID' value='{$background->charbgsID}' />\n";
-		$content .= "<input type='hidden' name='charbgName' value='{$background->NAME}' />\n";
+		$content .= "<input type='hidden' name='charbgID[$i]' value='{$background->charbgsID}' />\n";
+		$content .= "<input type='hidden' name='charbgName[$i]' value='{$background->NAME}' />\n";
+		
 		$content .= "<table>";
 		if ($background->HAS_SECTOR == 'Y') {
 			$content .= "<tr><th>Sector:</th></tr>";
-			$content .= "<tr><td><select name='sectorid'>";
+			$content .= "<tr><td><select name='sectorid[$i]'>";
 			$content .= "<option value='0' ";
 			if ($background->SECTOR_ID == 0)
 				$content .= "selected='selected'";
@@ -179,17 +197,22 @@ function get_editbackgrounds_tab($characterID) {
 			else if ($background->PENDING_DETAIL != "")
 				$content .= " (saved, awaiting approval)";
 			$content .= "</th></tr>";
-			$content .= "<tr><td><textarea name='pending' rows='5' cols='100'>";
-			if ($background->PENDING_DETAIL == "")
-				$content .= stripslashes($background->APPROVED_DETAIL);
+			$content .= "<tr><td><textarea name='pendingbg[$i]' rows='5' cols='100'>";
+			if (isset($pendingbg[$i]))
+				$content .= stripslashes($pendingbg[$i]);
 			else
-				$content .= stripslashes($background->PENDING_DETAIL);
+				if ($background->PENDING_DETAIL == "")
+					$content .= stripslashes($background->APPROVED_DETAIL);
+				else
+					$content .= stripslashes($background->PENDING_DETAIL);
 			$content .= "</textarea></td></tr>";
 		}
 		
-		$content .= "<tr><td><input type='submit' name='save_bgform{$i}' value='Save {$background->NAME}' /></td></tr>\n";
-		$content .= "</table></form></div>";
+		$content .= "<tr><td><input type='submit' name='save_bgform[$i]' value='Save {$background->NAME}' /></td></tr>\n";
+		$content .= "</table></div>\n";
+		$i++;
 	}
+	$content .= "</form>\n";
 	if (count($backgrounds) == 0) {
 		$content .= "<p>You have no backgrounds requiring explanation</p>";
 	}
@@ -204,24 +227,31 @@ function get_editmerits_tab($characterID) {
 	$characterID = establishCharacterID($character);
 	
 	/* Save Merits and Flaws */
-	if (isset($_REQUEST['meritID'])) {
-		$data = array (
-			'PENDING_DETAIL' => $_REQUEST['pending'],
-			'DENIED_DETAIL'  => ''
-		);
-		$wpdb->show_errors();
-		$result = $wpdb->update(GVLARP_TABLE_PREFIX . "CHARACTER_MERIT",
-			$data,
-			array (
-				'ID' => $_REQUEST['meritID']
-			)
-		);
+	if (isset($_REQUEST['save_meritform'])) {
+	
+		$meritids = $_REQUEST['meritID'];
+		$pendingmerit = $_REQUEST['pendingmerit'];
+		$namesmerit    = $_REQUEST['charmeritName'];
 		
-		if ($result) 			echo "<p style='color:green'>Updated {$_REQUEST['charmeritName']}</p>";
-		else if ($result === 0) echo "<p style='color:orange'>No updates made to {$_REQUEST['charmeritName']} background</p>";
-		else {
-			$wpdb->print_error();
-			echo "<p style='color:red'>Could not update {$_REQUEST['charmeritName']}</p>";
+		foreach ($_REQUEST['save_meritform'] as $id => $buttontitle) {
+			$data = array (
+				'PENDING_DETAIL' => $pendingmerit[$id],
+				'DENIED_DETAIL'  => ''
+			);
+			$wpdb->show_errors();
+			$result = $wpdb->update(GVLARP_TABLE_PREFIX . "CHARACTER_MERIT",
+				$data,
+				array (
+					'ID' => $meritids[$id]
+				)
+			);
+			
+			if ($result) 			echo "<p style='color:green'>Updated {$namesmerit[$id]}</p>";
+			else if ($result === 0) echo "<p style='color:orange'>No updates made to {$namesmerit[$id]} background</p>";
+			else {
+				$wpdb->print_error();
+				echo "<p style='color:red'>Could not update {$namesmerit[$id]}</p>";
+			}
 		}
 		
 	} 
@@ -240,18 +270,20 @@ function get_editmerits_tab($characterID) {
 	/* $content = "<p>SQL: $sql</p>"; */
 	
 	$merits = $wpdb->get_results($wpdb->prepare($sql, $characterID));
+	
 	$i = 0;
+	$content .= "<form name='extmeritform[$i]' method='post'>\n";
+	$content .= "<input type='hidden' name='charID' value='$characterID' />";
+	$content .= "<input type='hidden' name='tab' value='meritflaw' />\n";
+	
 	foreach ($merits as $merit) {
 	
 		$content .= "<p class='gvext_name'>" . $merit->NAME;
 		$content .= ($merit->COMMENT) ? " ({$merit->COMMENT})" : "";
 		$content .= "</p>\n<p class='gvext_ques'>" . stripslashes($merit->BACKGROUND_QUESTION) . "</p>\n";
 		$content .= "<div class='gvext_section'>";
-		$content .= "<form name='extmeritform{$i}' method='post'>\n";
-		$content .= "<input type='hidden' name='charID' value='$characterID' />";
-		$content .= "<input type='hidden' name='meritID' value='{$merit->meritID}' />\n";
-		$content .= "<input type='hidden' name='charmeritName' value='{$merit->NAME}' />\n";
-		$content .= "<input type='hidden' name='tab' value='meritflaw' />\n";
+		$content .= "<input type='hidden' name='meritID[$i]' value='{$merit->meritID}' />\n";
+		$content .= "<input type='hidden' name='charmeritName[$i]' value='{$merit->NAME}' />\n";
 		$content .= "<table>";
 
 		if (!empty($merit->APPROVED_DETAIL))
@@ -265,17 +297,23 @@ function get_editmerits_tab($characterID) {
 		else if ($merit->PENDING_DETAIL != "")
 			$content .= " (saved, awaiting approval)";
 		$content .= "</th></tr>";
-		$content .= "<tr><td><textarea name='pending' rows='5' cols='100'>";
-		if ($merit->PENDING_DETAIL == "")
-			$content .= stripslashes($merit->APPROVED_DETAIL);
+		$content .= "<tr><td><textarea name='pendingmerit[$i]' rows='5' cols='100'>";
+		if (isset($pendingmerit[$i]))
+			$content .= stripslashes($pendingmerit[$i]);
 		else
-			$content .= stripslashes($merit->PENDING_DETAIL);
+			if ($merit->PENDING_DETAIL == "")
+				$content .= stripslashes($merit->APPROVED_DETAIL);
+			else
+				$content .= stripslashes($merit->PENDING_DETAIL);
 		$content .= "</textarea></td></tr>";
 
 		
-		$content .= "<tr><td><input type='submit' name='save_meritform{$i}' value='Save {$merit->NAME}' /></td></tr>\n";
-		$content .= "</table></form></div>";
+		$content .= "<tr><td><input type='submit' name='save_meritform[$i]' value='Save {$merit->NAME}' /></td></tr>\n";
+		$content .= "</table></div>\n";
+		$i++;
 	}
+	$content .= "</form>\n";
+	
 	if (count($merits) == 0) {
 		$content .= "<p>You have no merits or flaws requiring explanation</p>";
 	}
@@ -291,53 +329,64 @@ function get_editmisc_tab($characterID) {
 	$characterID = establishCharacterID($character);
 	$wpdb->show_errors();
 	
+	$miscids  = $_REQUEST['miscID'];
+	$questids = $_REQUEST['questID'];
+	$pendingmisc = $_REQUEST['pendingmisc'];
+	$namesmisc    = $_REQUEST['charmiscTitle'];
+	
 	/* Save Misc Extended Background Answers */
 	if (isset($_REQUEST['miscID']) && $_REQUEST['miscID'] != "") {
-		/* update answer */
-		$data = array (
-			'PENDING_DETAIL' => $_REQUEST['pending'],
-			'DENIED_DETAIL'  => ''
-		);
-		
-		$result = $wpdb->update(GVLARP_TABLE_PREFIX . "CHARACTER_EXTENDED_BACKGROUND",
-			$data,
-			array (
-				'ID' => $_REQUEST['miscID']
-			)
-		);
-		
-		if ($result) 			echo "<p style='color:green'>Updated {$_REQUEST['charmiscTitle']}</p>";
-		else if ($result === 0) echo "<p style='color:orange'>No updates made to {$_REQUEST['charmiscTitle']} background</p>";
-		else {
-			$wpdb->print_error();
-			echo "<p style='color:red'>Could not update {$_REQUEST['charmiscTitle']}</p>";
+	
+		foreach ($_REQUEST['save_miscform'] as $id => $buttontext) {
+			/* update answer */
+			$data = array (
+				'PENDING_DETAIL' => $pendingmisc[$id],
+				'DENIED_DETAIL'  => ''
+			);
+			
+			$result = $wpdb->update(GVLARP_TABLE_PREFIX . "CHARACTER_EXTENDED_BACKGROUND",
+				$data,
+				array (
+					'ID' => $miscids[$id]
+				)
+			);
+			
+			if ($result) 			echo "<p style='color:green'>Updated {$namesmisc[$id]}</p>";
+			else if ($result === 0) echo "<p style='color:orange'>No updates made to {$namesmisc[$id]} background</p>";
+			else {
+				$wpdb->print_error();
+				echo "<p style='color:red'>Could not update {$namesmisc[$id]}</p>";
+			}
 		}
 		
 	} else {
 		if (isset($_REQUEST['miscformID'])) {
-			/* new answer */
-			$data = array (
-				'CHARACTER_ID'    => $_REQUEST['charID'],
-				'QUESTION_ID'     => $_REQUEST['questID'],
-				'APPROVED_DETAIL' => '',
-				'PENDING_DETAIL'  => $_REQUEST['pending'],
-				'DENIED_DETAIL'   => ''
-			);
-			$wpdb->insert(GVLARP_TABLE_PREFIX . "CHARACTER_EXTENDED_BACKGROUND", $data,
-				array (
-					'%d',
-					'%d',
-					'%s',
-					'%s',
-					'%s'
-				)
-			);
-			if ($wpdb->insert_id == 0) {
-				echo "<p style='color:red'><b>Error:</b> {$_REQUEST['charmiscTitle']} could not be saved (";
-				$wpdb->print_error();
-				echo ")</p>";
-			} else {
-				echo "<p style='color:green'>Saved answer '{$_REQUEST['charmiscTitle']}' for approval</p>";
+		
+			foreach ($_REQUEST['miscformID'] as $id => $buttontext) {
+				/* new answer */
+				$data = array (
+					'CHARACTER_ID'    => $_REQUEST['charID'],
+					'QUESTION_ID'     => $questids[$id],
+					'APPROVED_DETAIL' => '',
+					'PENDING_DETAIL'  => $pendingmisc[$id],
+					'DENIED_DETAIL'   => ''
+				);
+				$wpdb->insert(GVLARP_TABLE_PREFIX . "CHARACTER_EXTENDED_BACKGROUND", $data,
+					array (
+						'%d',
+						'%d',
+						'%s',
+						'%s',
+						'%s'
+					)
+				);
+				if ($wpdb->insert_id == 0) {
+					echo "<p style='color:red'><b>Error:</b> {$names[$id]} could not be saved (";
+					$wpdb->print_error();
+					echo ")</p>";
+				} else {
+					echo "<p style='color:green'>Saved answer '{$names[$id]}' for approval</p>";
+				}
 			}
 			
 		}
@@ -367,18 +416,19 @@ function get_editmisc_tab($characterID) {
 	
 	$questions = $wpdb->get_results($wpdb->prepare($sql, $characterID, $characterID));
 	$i = 0;
+	$content .= "<form name='extmiscform' method='post'>\n";
+	$content .= "<input type='hidden' name='tab' value='misc' />\n";
+	$content .= "<input type='hidden' name='charID' value='$characterID' />";
+	
 	foreach ($questions as $question) {
 	
 		$content .= "<p class='gvext_name'>" . $question->TITLE . "</p>\n";
 		$content .= "<p class='gvext_ques'>" . stripslashes($question->BACKGROUND_QUESTION) . "</p>\n";
 		$content .= "<div class='gvext_section'>";
-		$content .= "<form name='extmiscform{$i}' method='post'>\n";
-		$content .= "<input type='hidden' name='charID' value='$characterID' />";
-		$content .= "<input type='hidden' name='miscID' value='{$question->miscID}' />\n";
-		$content .= "<input type='hidden' name='miscformID' value='{$i}' />\n";
-		$content .= "<input type='hidden' name='questID' value='{$question->questID}' />\n";
-		$content .= "<input type='hidden' name='charmiscTitle' value='{$question->TITLE}' />\n";
-		$content .= "<input type='hidden' name='tab' value='misc' />\n";
+		$content .= "<input type='hidden' name='miscID[$i]' value='{$question->miscID}' />\n";
+		$content .= "<input type='hidden' name='miscformID[$i]' value='{$i}' />\n";
+		$content .= "<input type='hidden' name='questID[$i]' value='{$question->questID}' />\n";
+		$content .= "<input type='hidden' name='charmiscTitle[$i]' value='{$question->TITLE}' />\n";
 		$content .= "<table>";
 
 		if (!empty($question->APPROVED_DETAIL))
@@ -392,17 +442,22 @@ function get_editmisc_tab($characterID) {
 		else if ($question->PENDING_DETAIL != "")
 			$content .= " (saved, awaiting approval)";
 		$content .= "</th></tr>";
-		$content .= "<tr><td><textarea name='pending' rows='5' cols='100'>";
-		if ($question->PENDING_DETAIL == "")
-			$content .= stripslashes($question->APPROVED_DETAIL);
+		$content .= "<tr><td><textarea name='pendingmisc[$i]' rows='5' cols='100'>";
+		if (isset($pendingmisc[$i]))
+			$content .= stripslashes($pendingmisc[$i]);
 		else
-			$content .= stripslashes($question->PENDING_DETAIL);
+			if ($question->PENDING_DETAIL == "")
+				$content .= stripslashes($question->APPROVED_DETAIL);
+			else
+				$content .= stripslashes($question->PENDING_DETAIL);
 		$content .= "</textarea></td></tr>";
 
 		
-		$content .= "<tr><td><input type='submit' name='save_miscform{$i}' value='Save {$question->TITLE}' /></td></tr>\n";
-		$content .= "</table></form></div>";
+		$content .= "<tr><td><input type='submit' name='save_miscform[$i]' value='Save {$question->TITLE}' /></td></tr>\n";
+		$content .= "</table></div>\n";
+		$i++;
 	}
+	$content .= "</form>\n";
 	if (count($questions) == 0) {
 		$content .= "<p>There are no extended background questions to answer.</p>";
 	}
