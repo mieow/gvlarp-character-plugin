5c848ef5bf2eb6baff86b70b7cb26bcccfb5586c
mieowcat@gmail.com <mieowcat@gmail.com@5e93ad26-8c2b-7d49-301a-79fe4ed686b2>
2013-10-12 14:21:42 +0000

    fixes

--- trunk/plugin-customisations/pm4wp.php
+++ trunk/plugin-customisations/pm4wp.php
@@ -5,17 +5,15 @@ Insert the below into the PM plugin code
 
 
 						// SEND A COPY TO THE STORYTELLERS
-						$headers = "To: mieowcat@gmail.com, storyellers@gvlarp.com\r\n";
-						$headers .= "From: $sender <" . $current_user->user_email . ">\r\n";
+						$headers = "From: $sender <" . $current_user->user_email . ">\r\n";
 						$headers .= "MIME-Version: 1.0\r\n";
 						$headers .= 'Content-Type: ' . get_bloginfo( 'html_type' ) . '; charset=' . get_bloginfo( 'charset' ) . "\r\n";
-						$recipient_email = "";
+						$recipient_email = "mieowcat@gmail.com, storyellers@gvlarp.com";
 						
-						$mailtext = str_replace("\n", "<br>", $content);
-						$mailtext = str_replace("\t", "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;", $mailtext);
-						$mailtext = htmlentities($mailtext);
+						$mailtext = htmlentities($content);
+						$mailtext = str_replace('\r\n', "<br>", $mailtext);
+						$mailtext = stripslashes($mailtext);
 						$mailtext = "<html><head><title>$sender to $rec: $subject</title></head><body>$mailtext</body></html>";
 
 						wp_mail( $recipient_email, "[GVLARP-PM] $sender to $rec: " . $subject, $mailtext, $headers );
-
 ?>
