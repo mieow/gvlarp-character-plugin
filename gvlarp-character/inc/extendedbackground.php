702b6dbcb10a45750f6fa92faf52e4c89a0b1d93
mieowcat@gmail.com <mieowcat@gmail.com@5e93ad26-8c2b-7d49-301a-79fe4ed686b2>
2014-02-07 14:30:57 +0000

    Improve output of approved backgrounds

--- gvlarp-character/inc/extendedbackground.php
+++ gvlarp-character/inc/extendedbackground.php
@@ -152,7 +152,7 @@ function get_editbackgrounds_tab($characterID) {
 		$content .= ($background->COMMENT) ? " ({$background->COMMENT})" : "";
 		$content .= "</p>\n";
 		if (!empty($background->BACKGROUND_QUESTION))
-			$content .= "<div class='gvext_ques'>" . wpautop($background->BACKGROUND_QUESTION) . "</div>\n";
+			$content .= "<div class='gvext_ques'>" . wpautop(stripslashes($background->BACKGROUND_QUESTION)) . "</div>\n";
 		$content .= "<div class='gvext_section'>";
 		$content .= "<input type='hidden' name='charbgID[$i]' value='{$background->charbgsID}' />\n";
 		$content .= "<input type='hidden' name='charbgName[$i]' value='{$background->NAME}' />\n";
@@ -187,9 +187,9 @@ function get_editbackgrounds_tab($characterID) {
 		
 		if ($background->BACKGROUND_QUESTION != '') {
 			if (!empty($background->APPROVED_DETAIL))
-				$content .= "<tr><th>Approved Description</th></tr><tr><td class='gvext_approved'>" . wpautop($background->APPROVED_DETAIL) . "</td></tr>";
+				$content .= "<tr><th>Approved Description</th></tr><tr><td class='gvext_approved'>" . wpautop(stripslashes($background->APPROVED_DETAIL)) . "</td></tr>";
 			if ($background->DENIED_DETAIL != "") {
-				$content .= "<tr><th>Description Denied</th></tr><tr><td class='gvext_denied'>" . wpautop($background->DENIED_DETAIL) . "</td></tr>\n";
+				$content .= "<tr><th>Description Denied</th></tr><tr><td class='gvext_denied'>" . wpautop(stripslashes($background->DENIED_DETAIL)) . "</td></tr>\n";
 			}
 			$content .= "<tr><th>Update Description";
 			if ($background->DENIED_DETAIL != "")
@@ -280,16 +280,16 @@ function get_editmerits_tab($characterID) {
 	
 		$content .= "<p class='gvext_name'>" . $merit->NAME;
 		$content .= ($merit->COMMENT) ? " ({$merit->COMMENT})" : "";
-		$content .= "</p>\n<div class='gvext_ques'>" . wpautop($merit->BACKGROUND_QUESTION) . "</div>\n";
+		$content .= "</p>\n<div class='gvext_ques'>" . wpautop(stripslashes($merit->BACKGROUND_QUESTION)) . "</div>\n";
 		$content .= "<div class='gvext_section'>";
 		$content .= "<input type='hidden' name='meritID[$i]' value='{$merit->meritID}' />\n";
 		$content .= "<input type='hidden' name='charmeritName[$i]' value='{$merit->NAME}' />\n";
 		$content .= "<table>";
 
 		if (!empty($merit->APPROVED_DETAIL))
-			$content .= "<tr><th>Approved Description</th></tr><tr><td class='gvext_approved'>" . wpautop($merit->APPROVED_DETAIL) . "</td></tr>";
+			$content .= "<tr><th>Approved Description</th></tr><tr><td class='gvext_approved'>" . wpautop(stripslashes($merit->APPROVED_DETAIL)) . "</td></tr>";
 		if ($merit->DENIED_DETAIL != "") {
-			$content .= "<tr><th>Description Denied</th></tr><tr><td class='gvext_denied'>" . wpautop($merit->DENIED_DETAIL) . "</td></tr>\n";
+			$content .= "<tr><th>Description Denied</th></tr><tr><td class='gvext_denied'>" . wpautop(stripslashes($merit->DENIED_DETAIL)) . "</td></tr>\n";
 		}
 		$content .= "<tr><th>Update Description";
 		if ($merit->DENIED_DETAIL != "")
@@ -422,7 +422,7 @@ function get_editmisc_tab($characterID) {
 	foreach ($questions as $question) {
 	
 		$content .= "<p class='gvext_name'>" . $question->TITLE . "</p>\n";
-		$content .= "<div class='gvext_ques'>" . wpautop($question->BACKGROUND_QUESTION) . "</div>\n";
+		$content .= "<div class='gvext_ques'>" . wpautop(stripslashes($question->BACKGROUND_QUESTION)) . "</div>\n";
 		$content .= "<div class='gvext_section'>";
 		$content .= "<input type='hidden' name='miscID[$i]' value='{$question->miscID}' />\n";
 		$content .= "<input type='hidden' name='miscformID[$i]' value='{$i}' />\n";
@@ -431,9 +431,9 @@ function get_editmisc_tab($characterID) {
 		$content .= "<table>";
 
 		if (!empty($question->APPROVED_DETAIL))
-			$content .= "<tr><th>Approved Description</th></tr><tr><td class='gvext_approved'>" . wpautop($question->APPROVED_DETAIL) . "</td></tr>";
+			$content .= "<tr><th>Approved Description</th></tr><tr><td class='gvext_approved'>" . wpautop(stripslashes($question->APPROVED_DETAIL)) . "</td></tr>";
 		if ($question->DENIED_DETAIL != "") {
-			$content .= "<tr><th>Description Denied</th></tr><tr><td class='gvext_denied'>" . wpautop($question->DENIED_DETAIL) . "</td></tr>\n";
+			$content .= "<tr><th>Description Denied</th></tr><tr><td class='gvext_denied'>" . wpautop(stripslashes($question->DENIED_DETAIL)) . "</td></tr>\n";
 		}
 		$content .= "<tr><th>Update Description";
 		if ($question->DENIED_DETAIL != "")
