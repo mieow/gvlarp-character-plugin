19e487fe9ba625e464ecac3294af59722bfdec11
mieowcat@gmail.com <mieowcat@gmail.com@5e93ad26-8c2b-7d49-301a-79fe4ed686b2>
2014-02-21 16:42:08 +0000

    GVTheme: added colour and gradient options - blue, red and green

--- GVTheme/theme-options.php
+++ GVTheme/theme-options.php
@@ -16,45 +16,15 @@ function register_gv_settings() {
 	/* Main Settings */
 	add_settings_section('gv_options_section_main', 'Main Settings', 'gv_options_section_main_text', 'gvadmin_slug');
 	add_settings_field('gv_copyright', 'Copyright Notice', 'gv_options_input_copyright', 'gvadmin_slug', 'gv_options_section_main');
-	add_settings_field('gv_credits',   'Website Credits',   'gv_options_input_credits',   'gvadmin_slug', 'gv_options_section_main');
-	
-	/* By Role Settings */
-/* 	add_settings_section('gv_options_section_roles', 'Role Settings', 'gv_options_section_role_text', 'gvadmin_slug');
-	foreach( $wp_roles->role_objects as $role => $roleclass ) {
-  		add_settings_field('gv_rolelink_' . $role, $role . " Link Name:", "gv_options_input_rolelink", 'gvadmin_slug', 'gv_options_section_roles', $role);
-  		add_settings_field('gv_roleurl_' . $role, $role . " Page URL:", "gv_options_input_roleurl", 'gvadmin_slug', 'gv_options_section_roles', $role);
-	} */
-	
+	add_settings_field('gv_credits',   'Website Credits',  'gv_options_input_credits',   'gvadmin_slug', 'gv_options_section_main');
+	add_settings_field('gv_colours',   'Theme Colour',     'gv_options_input_colour',    'gvadmin_slug', 'gv_options_section_main');
+	add_settings_field('gv_gradient',  'Button/Nav Gradient', 'gv_options_input_gradient', 'gvadmin_slug', 'gv_options_section_main');
+		
 } 
 add_action( 'admin_init', 'register_gv_settings' );
 
 function gv_options_section_main_text() {
 	echo '<p>General settings for theme.</p>';
-}
-function gv_options_section_role_text() {
-	global $wp_roles;
-	
-	echo '<p>Role settings for theme.</p><table>';
-	
-	/* $methods = get_class_methods($wp_roles);
-	foreach ( $methods as $method ) {
-		echo "<p>Method: $method</p>";
-	} 
-	$classvars = get_class_vars(get_class($wp_roles));
-	foreach ( $classvars as $name => $value ) {
-		echo "<p>Var: $name - $value";
-		echo "</p>";
-	}
-	
-	foreach( $wp_roles->role_names as $test ) {
-		echo "<p>Names: $test</p>";
-	}
-	foreach( $wp_roles->role_objects as $myarray => $test ) {
-		echo "<p>$myarray | " . get_class($test) . "</p>";
-	} */
-
-
-
 }
 function gv_options_input_copyright() {
 	$options = get_option('gv_options');
@@ -64,14 +34,22 @@ function gv_options_input_credits() {
 	$options = get_option('gv_options');
 	echo "<input id='gv_credits' name='gv_options[credits]' size='40' type='text' value='{$options['credits']}' />";
 }
-function gv_options_input_rolelink($role) {
+function gv_options_input_colour() {
 	$options = get_option('gv_options');
-	echo "<input id='gv_rolelink_" . $role , "' name='gv_options[rolelink_" . $role . "]' size='20' type='text' value='{$options['rolelink_' . $role]}' />";
+	echo "<select id='gv_colours' name='gv_options[colours]'>\n";
+	echo "<option value='red' " . selected($options['colours'], 'red', false) . ">Red</option>";
+	echo "<option value='blue' " . selected($options['colours'], 'blue', false) . ">Blue</option>";
+	echo "<option value='green' " . selected($options['colours'], 'green', false) . ">Green</option>";
+	echo "</select>";
 }
-function gv_options_input_roleurl($role) {
+function gv_options_input_gradient() {
 	$options = get_option('gv_options');
-	echo "<input id='gv_roleurl_" . $role , "' name='gv_options[roleurl_" . $role . "]' size='50' type='text' value='{$options['roleurl_' . $role]}' />";
+	echo "<select id='gv_gradient' name='gv_options[gradient]'>\n";
+	echo "<option value='flat' " . selected($options['gradient'], 'flat', false) . ">Flat</option>";
+	echo "<option value='gradient' " . selected($options['gradient'], 'gradient', false) . ">Gradient</option>";
+	echo "</select>";
 }
+
 
 function gvadmin_function() {
 
@@ -98,12 +76,9 @@ function gv_options_validate($input) {
 	$options = get_option('gv_theme_options');
 	
 	$options['copyright'] = trim($input['copyright']);
-	$options['credits'] = trim($input['credits']);
-	
-	foreach( $wp_roles->role_objects as $role => $roleclass ) {
-  		$options['rolelink_' . $role] = trim($input['rolelink_' . $role]);
-  		$options['roleurl_' . $role] = trim($input['roleurl_' . $role]);
-	}
+	$options['credits']   = trim($input['credits']);
+	$options['colours']   = trim($input['colours']);
+	$options['gradient']  = trim($input['gradient']);
 	
 	return $options;
 }
