6f660f100a501e265d3c2535659a3232ef41e7a3
mieowcat@gmail.com <mieowcat@gmail.com@5e93ad26-8c2b-7d49-301a-79fe4ed686b2>
2014-02-24 14:16:25 +0000

    plugin customisations: added more

---
+++ plugin-customisations/alo-easymail-themes/xp-newsletter-template.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<style type="text/css">
+
+a:link    {color:#CCCCCC;}  /* unvisited link */
+a:visited {color:#444444;}  /* visited link */
+a:hover   {color:red;}      /* mouse over link */
+a:active  {color:darkred;}  /* selected link */ 
+
+</style>
+
+<title>[XP] [TITLE]</title>
+
+</head>
+<body style="font-family:Arial">
+
+<div class="heading">
+<h1>Experience Notification</h1>
+</div>
+<div style="text-align:right;">
+<strong>[DATE]</strong>
+</div>
+<div style="padding:10px;">
+<div class="sidebox" style="width:30%;float:right;">
+
+<p><strong>Newsletter</strong></p>
+<ul><li>MMM YYYY</li></ul>
+<p><strong>Upcoming Dates</strong></p>
+<ul>[GV_CALENDAR]</ul>
+
+</div>
+<div class="messages" style="width:67%;border-width:2px;border-color:darkred;border-right-style:solid;border-top-style:dotted;">
+
+<h2 style="border-width:1px;border-color:darkred;border-bottom-style:solid;width:50%;color:#787878;">Character Update</h2>
+<p>You have <b>[GV_XPTOTAL]</b> experience points to spend.</p>
+
+<h2 style="border-width:1px;border-color:darkred;border-bottom-style:solid;width:50%;color:#787878;">Storyteller Message</h2>
+[CONTENT]
+
+</div>
+
+</div>
+<div style="margin-top:20px;">
+[USER-UNSUBSCRIBE]
+</div>
+
+</body>
+</html>
