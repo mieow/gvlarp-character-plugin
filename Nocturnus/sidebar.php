066ffd76757c08c0eee3eb93de1e3f392fd392c1
mieowcat@gmail.com <mieowcat@gmail.com@5e93ad26-8c2b-7d49-301a-79fe4ed686b2>
2014-02-24 16:51:08 +0000

    Can now choose sidebar location

--- Nocturnus/sidebar.php
+++ Nocturnus/sidebar.php
@@ -3,7 +3,7 @@
 			<h2 class="widgettitle">Search</h2>
 			<?php get_search_form( ); ?> 
 		</div>
-		<ul>
+		<ul id="nsidebar1">
 			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('nocturnus_sidebar') ) : ?>
 			<?php endif; ?>
 		</ul>
