00c890e002ed808d33ab4fdfe535f1b9dfebbddf
mieowcat@gmail.com <mieowcat@gmail.com@5e93ad26-8c2b-7d49-301a-79fe4ed686b2>
2014-03-10 11:41:11 +0000

    Themes: Theme folder updates

--- trunk/GVTheme/searchform.php
+++
@@ -1,6 +0,0 @@
-<form action="/" method="get">
-    <fieldset>
-        <input type="text" name="s" id="search" class="searchbox" value="<?php the_search_query(); ?>" />
-        <input type="image" alt="Search" class="searchsubmit" src="<?php echo get_template_directory_uri(); ?>/images/search.png" />
-    </fieldset>
-</form>
